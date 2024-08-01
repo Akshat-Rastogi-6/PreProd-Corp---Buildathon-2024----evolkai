430aee95c9a16d6ea2fec7b3f7a0f6b6883dff45
Shubh Gupta <shubhorai12@gmail.com>
2024-08-01 15:19:32 +0530

    updated index file added UI

--- index.php
+++ index.php
@@ -1,19 +1,50 @@
-<!-- index.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing Page</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Evolkai - Accurate</title>
 </head>
 <body>
     <header>
-        <h1>Welcome to AutoML App</h1>
+        <nav>
+            <img src="assets/evolkai.png" alt="" class="logo">
+        </nav>
     </header>
-    <main>
-        <a href="signup.php">Sign Up</a>
-        <a href="signin.php">Sign In</a>
-    </main>
+    <section class="main">
+        <h1 class="heading1">ACCURATE</h1>
+        <div class="paragraph">
+            <p>
+                Discover the ultimate data analysis tool that transforms any dataset into insightful metrics, delivering accuracy, precision, recall scores, and comprehensive survey results instantly. Unveil the full potential of your data today!
+            </p>
+        </div>
+
+        <button class="label">Sign Up</button>
+    </section>
+    
+    <section class="second_screen">
+        <div class="card">
+            <form>
+                <label for="user-id">User ID</label>
+                <input type="text" id="user-id" name="user-id">
+                
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name">
+                
+                <label for="email">Email Address</label>
+                <input type="email" id="email" name="email">
+                
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password">
+                
+                <label for="confirm-password">Confirm Password</label>
+                <input type="password" id="confirm-password" name="confirm-password">
+                
+                <button type="submit">SIGN UP</button>
+                <p>Already have an account? <a href="signin.php">Sign in!</a></p>
+            </form>
+        </div>
+    </section>
 </body>
 </html>
